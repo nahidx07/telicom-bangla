8f11356ccfc84c9d14cbecf9f77235958954e2bb
nahidx07 <muhammadnahidhossen70@gmail.com>
2026-01-20 02:29:52 +0600

    Update RegisterPage.tsx

--- pages/auth/RegisterPage.tsx
+++ pages/auth/RegisterPage.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Phone, User as UserIcon, Tag, ArrowRight, Smartphone, Mail } from 'lucide-react';
